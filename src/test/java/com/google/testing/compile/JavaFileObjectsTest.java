4dcf1db2bbd345bfb94ceec610b98604f4e85d7b
Gregory Kick <gk5885@gmail.com>
2013-08-27 23:37:16 -0500

    Update JavaFileObjects.forResource to support jar URLs

---
+++ src/test/java/com/google/testing/compile/JavaFileObjectsTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2013 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.testing.compile;
+
+import static javax.tools.JavaFileObject.Kind.CLASS;
+import static org.truth0.Truth.ASSERT;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.tools.JavaFileObject;
+
+import org.junit.Test;
+
+import com.google.common.io.Resources;
+
+/**
+ *  Tests {@link JavaFileObjects}.
+ *
+ *  @author Gregory Kick
+ */
+public class JavaFileObjectsTest {
+  @Test public void forResource_inJarFile() throws URISyntaxException, IOException {
+    JavaFileObject resourceInJar = JavaFileObjects.forResource("java/lang/Object.class");
+    ASSERT.that(resourceInJar.getKind()).isEqualTo(CLASS);
+    ASSERT.that(resourceInJar.toUri())
+        .isEqualTo(Resources.getResource("java/lang/Object.class").toURI());
+    ASSERT.that(resourceInJar.getName())
+        .isEqualTo(Resources.getResource("java/lang/Object.class").toURI().getSchemeSpecificPart());
+  }
+}

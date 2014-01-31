279c458130b689e8cf6c4440b23cb6a37fcb914f
Gregory Kick <gk5885@gmail.com>
2014-01-31 15:56:38 -0600

    Add the compilation rule to enable unit tests that can utilize Elements and TypeMirrors

---
+++ src/main/java/com/google/testing/compile/CompilationRule.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.testing.compile;
+
+import static com.google.common.base.Preconditions.checkState;
+
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+import org.junit.Rule;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.JUnit4;
+import org.junit.runners.model.Statement;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.testing.compile.Compilation.Result;
+
+/**
+ * A {@link JUnit4} {@link Rule} that executes tests such that an instances of {@link Elements}
+ * and {@link Types} are available during execution.
+ *
+ * <p>To use this rule in a test, just add the following field: <pre>   {@code
+ *   @Rule public CompilationRule compilationRule = new CompilationRule();}
+ *
+ * @author Gregory Kick
+ */
+public final class CompilationRule implements TestRule {
+  private Elements elements;
+  private Types types;
+
+  @Override
+  public Statement apply(final Statement base, Description description) {
+    return new Statement() {
+      @Override public void evaluate() throws Throwable {
+        final AtomicReference<Throwable> thrown = new AtomicReference<Throwable>();
+        Result result = Compilation.compile(ImmutableList.of(new AbstractProcessor() {
+          @Override
+          public SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.latest();
+          }
+
+          @Override
+          public Set<String> getSupportedAnnotationTypes() {
+            return ImmutableSet.of("*");
+          }
+
+          @Override
+          public synchronized void init(ProcessingEnvironment processingEnv) {
+            super.init(processingEnv);
+            elements = processingEnv.getElementUtils();
+            types = processingEnv.getTypeUtils();
+          }
+
+          @Override
+          public boolean process(Set<? extends TypeElement> annotations,
+              RoundEnvironment roundEnv) {
+            // just run the test on the last round after compilation is over
+            if (roundEnv.processingOver()) {
+              try {
+                base.evaluate();
+              } catch (Throwable e) {
+                thrown.set(e);
+              }
+            }
+            return false;
+          }
+        }),
+        // just compile _something_
+        ImmutableList.of(JavaFileObjects.forSourceLines("Dummy", "final class Dummy {}")));
+        checkState(result.successful(), result);
+        Throwable t = thrown.get();
+        if (t != null) {
+          throw t;
+        }
+      }
+    };
+  }
+
+  public Elements getElements() {
+    checkState(elements != null, "Not running within the rule");
+    return elements;
+  }
+
+  public Types getTypes() {
+    checkState(elements != null, "Not running within the rule");
+    return types;
+  }
+}
---
+++ src/test/java/com/google/testing/compile/CompilationRuleTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2014 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.testing.compile;
+
+import static org.truth0.Truth.ASSERT;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests the {@link CompilationRule} by applying it to this test.
+ */
+@RunWith(JUnit4.class)
+public class CompilationRuleTest {
+  @Rule public CompilationRule compilationRule = new CompilationRule();
+
+  private final AtomicInteger executions = new AtomicInteger();
+
+  @Test public void testMethodsExecuteExactlyOnce() {
+    ASSERT.that(executions.getAndIncrement()).is(0);
+  }
+
+  @Before /* we also make sure that getElements works in a @Before method */
+  @Test public void getElements() {
+    ASSERT.that(compilationRule.getElements()).isNotNull();
+  }
+
+  @Before /* we also make sure that getTypes works in a @Before method */
+  @Test public void getTypes() {
+    ASSERT.that(compilationRule.getTypes()).isNotNull();
+  }
+
+  /**
+   * Do some non-trivial operation with {@link Element} instances because they stop working after
+   * compilation stops.
+   */
+  @Test public void elementsAreValidAndWorking() {
+    Elements elements = compilationRule.getElements();
+    TypeElement stringElement = elements.getTypeElement(String.class.getName());
+    ASSERT.that(stringElement.getEnclosingElement())
+        .isEqualTo(elements.getPackageElement("java.lang"));
+  }
+
+  /**
+   * Do some non-trivial operation with {@link TypeMirror} instances because they stop working after
+   * compilation stops.
+   */
+  @Test public void typeMirrorsAreValidAndWorking() {
+    Elements elements = compilationRule.getElements();
+    Types types = compilationRule.getTypes();
+    DeclaredType arrayListOfString = types.getDeclaredType(
+        elements.getTypeElement(ArrayList.class.getName()),
+        elements.getTypeElement(String.class.getName()).asType());
+    DeclaredType listOfExtendsObjectType = types.getDeclaredType(
+        elements.getTypeElement(List.class.getName()),
+        types.getWildcardType(elements.getTypeElement(Object.class.getName()).asType(), null));
+    ASSERT.that(types.isAssignable(arrayListOfString, listOfExtendsObjectType)).isTrue();
+  }
+}

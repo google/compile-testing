5c80dbe4df7e4fe5d37e64f71f9f20b980cb8f51
Andreas Deininger <adeininger@gmail.com>
2021-04-01 02:25:00 +0200

    CONTRIBUTING.md: correct Travis CI link
    Switch from http to https (license in source file, ...)

--- src/main/java/com/google/testing/compile/CompileTester.java
+++ src/main/java/com/google/testing/compile/CompileTester.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -147,7 +147,7 @@ public interface CompileTester {
   public interface GeneratedPredicateClause<T> {
     /**
      * Checks that a source file with an equivalent
-     * <a href="http://en.wikipedia.org/wiki/Abstract_syntax_tree">AST</a> was generated for each of
+     * <a href="https://en.wikipedia.org/wiki/Abstract_syntax_tree">AST</a> was generated for each of
      * the given {@linkplain JavaFileObject files}.
      */
     @CanIgnoreReturnValue
